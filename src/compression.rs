1fa8d3cf482c043d07940ead9c36e70dbcdfdd3c
frederic wagner <frederic.wagner@univ-grenoble-alpes.fr>
2023-01-05 14:12:16 +0100

    tile ways

--- src/compression.rs
+++ src/compression.rs
@@ -6,7 +6,7 @@ use crate::{Node, NodeId, TileKey, WayId};
 pub struct CompressedMap {
     binary_ways: Vec<u8>,
     start_coordinates: (f64, f64),
-    first_way_in_each_tile: Vec<usize>,
+    tiles_sizes_prefix: Vec<usize>,
     tiles_per_line: usize,
     side: f64,
 }
@@ -39,7 +39,6 @@ impl CompressedMap {
             .unwrap();
         for y in ymin..=ymax {
             for x in xmin..=xmax {
-                tiles_sizes_prefix.push(processed_ways_count);
                 if let Some(tile_ways) = tiles.get(&(x, y)) {
                     for way_id in tile_ways {
                         let way = &ways[*way_id];
@@ -49,12 +48,11 @@ impl CompressedMap {
                                 .flat_map(|node_id| nodes[*node_id].encode(x, y, side)),
                         );
                         ids_changes.insert(way_id, processed_ways_count);
-                        processed_ways_count += 1;
                     }
                 }
+                tiles_sizes_prefix.push(binary_ways.len());
             }
         }
-        tiles_sizes_prefix.push(processed_ways_count);
 
         for street in streets.values_mut() {
             let new_street = street
@@ -66,7 +64,7 @@ impl CompressedMap {
         CompressedMap {
             binary_ways,
             start_coordinates: (xmin as f64 * side, ymin as f64 * side),
-            first_way_in_each_tile: tiles_sizes_prefix,
+            tiles_sizes_prefix,
             tiles_per_line: xmax + 1 - xmin,
             side,
         }
@@ -77,46 +75,57 @@ impl CompressedMap {
         let mut nodes = Vec::new();
         let mut seen_nodes = HashMap::new();
         let mut ways = Vec::new();
-        while position < self.binary_ways.len() {
-            let way_length = self.binary_ways[position];
-            position += 1;
-            let tile_number = match self.first_way_in_each_tile.binary_search(&ways.len()) {
-                Ok(i) => {
-                    // we need the rightmost match
-                    self.first_way_in_each_tile[i..]
-                        .iter()
-                        .enumerate()
-                        .take_while(|&(_, s)| *s == ways.len())
-                        .last()
-                        .map(|(i, _)| i)
-                        .unwrap()
-                        + i
-                }
-                Err(i) => i - 1,
-            };
+        for tile_number in 0..self.tiles_sizes_prefix.len() {
             let tile_x = tile_number % self.tiles_per_line;
             let tile_y = tile_number / self.tiles_per_line;
-            let mut way = Vec::new();
-            for i in 0..way_length {
-                let cx = self.binary_ways[position + 2 * i as usize];
-                let cy = self.binary_ways[position + 2 * i as usize + 1];
-                let x = self.start_coordinates.0
-                    + tile_x as f64 * self.side
-                    + cx as f64 / 255. * self.side;
-                let y = self.start_coordinates.1
-                    + tile_y as f64 * self.side
-                    + cy as f64 / 255. * self.side;
-                let node = Node::new(x, y);
-                let node_id = *seen_nodes.entry(node).or_insert_with(|| {
-                    let new_id = nodes.len();
-                    nodes.push(node);
-                    new_id
-                });
-                way.push(node_id);
+            for way_nodes in self.tile_ways(tile_x, tile_y) {
+                let mut way = Vec::new();
+                for node in way_nodes {
+                    let node_id = *seen_nodes.entry(node).or_insert_with(|| {
+                        let new_id = nodes.len();
+                        nodes.push(node);
+                        new_id
+                    });
+                    way.push(node_id);
+                }
+                ways.push(way);
             }
-            position += 2 * way_length as usize;
-            ways.push(way);
         }
         (nodes, ways)
+    }
+    pub fn tile_ways(&self, tile_x: usize, tile_y: usize) -> impl Iterator<Item = Vec<Node>> + '_ {
+        let tile_number = tile_y * self.tiles_per_line + tile_x;
+        let tile_x = tile_number % self.tiles_per_line;
+        let tile_y = tile_number / self.tiles_per_line;
+        let binary_end = self.tiles_sizes_prefix[tile_number];
+        let binary_start = tile_number
+            .checked_sub(1)
+            .map(|i| self.tiles_sizes_prefix[i])
+            .unwrap_or_default();
+        let mut binary_tile = &self.binary_ways[binary_start..binary_end];
+        std::iter::from_fn(move || {
+            if let Some((way_length, remainder)) = binary_tile.split_first() {
+                let (binary_way, remainder) = remainder.split_at(2 * *way_length as usize);
+                let r: &[u8] = binary_way;
+                binary_tile = remainder;
+                Some(
+                    binary_way
+                        .iter()
+                        .tuples()
+                        .map(|(cx, cy)| {
+                            let x = self.start_coordinates.0
+                                + tile_x as f64 * self.side
+                                + *cx as f64 / 255. * self.side;
+                            let y = self.start_coordinates.1
+                                + tile_y as f64 * self.side
+                                + *cy as f64 / 255. * self.side;
+                            Node::new(x, y)
+                        })
+                        .collect::<Vec<_>>(),
+                )
+            } else {
+                None
+            }
+        })
     }
 }
